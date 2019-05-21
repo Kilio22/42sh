a5b865992305de94cc4c7cb44ed27e3804abd3bf
HeyShafty <nathan.lecorchet@epitech.eu>
2019-05-21 17:16:55 +0200

    struct token_node is cleared on tokenizer.h

--- lib/include/tokenizer.h
+++ lib/include/tokenizer.h
@@ -8,10 +8,13 @@
 #ifndef TOKENIZER_H_
 #define TOKENIZER_H_
 
+#include "delimiters.h"
+
 struct token_node {
     struct token_node *next;
     struct token_node *prev;
     char *content;
+    enum delimiter_ids id;
 };
 
 #endif /* !TOKENIZER_H_ */
