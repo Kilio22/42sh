2e4defc94725c264a66150dd4587b46c962ac040
Kylian Balan <kylian.balan@epitech.eu>
2019-05-21 18:16:52 +0200

    add find token type

--- src/tokenizer/create_token_list_from_line.c
+++ src/tokenizer/create_token_list_from_line.c
@@ -7,11 +7,26 @@
 
 #include <string.h>
 #include <stdlib.h>
+#include "tokenizer.h"
+#include "delimiters.h"
+
+int find_token_type(char *line)
+{
+    for (size_t i = 0; delim_tab[i]; i++) {
+        if (!strncmp(line, delim_tab[i]->delim, strlen(delim_tab[i]->delim)))
+            return delim_tab[i]->type;
+    }
+    return T_WITHOUT;
+}
 
 struct token_node *create_token_list_from_line(char *line)
 {
     struct token_node *head = NULL;
+    size_t i = 0;
 
+    while (line[i]) {
+
+    }
     free(line);
     return head;
 }
