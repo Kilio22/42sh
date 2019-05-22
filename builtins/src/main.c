5996a71701d1de334c2c378441a5e8da9d6526c5
Antoine Gosselin <agosselin@localhost.localdomain>
2019-05-22 16:48:59 +0200

    add builtins, where, which

--- builtins/src/main.c
+++ builtins/src/main.c
@@ -5,18 +5,175 @@
 ** Main function
 */
 
-#include <sys/types.h>
 #include <unistd.h>
+#include <string.h>
 #include <stdlib.h>
-#include "my.h"
+#include <stdbool.h>
 #include "my_stdio.h"
 #include "my_string.h"
-#include "minishell.h"
+
+const char *my_builtins[][2] = {
+    {"cd", "change the current working directory"},
+    {"env", "display the env"},
+    {"echo", "display the given arguments"},
+    {"exit", "exit the shell with the given return value"},
+    {"snake", "launch the snake game"},
+    {"where", "display all known instances of command."},
+    {"which", "display the real command that the shell execute"},
+    {"setenv", "set a new env variable or change an existing one"},
+    {"builtins", "display all shell built-in"},
+    {"unsetenv", "unset one or more env variable(s)"}
+};
+
+void display_builtins(void)
+{
+    for (int i = 0; i < 10; i++) {
+        my_printf("%s", my_builtins[i][0]);
+        if (my_strlen(my_builtins[i][0]) < 8)
+            my_printf("\t");
+        my_printf(" :\t%s\n", my_builtins[i][1]);
+    }
+}
+
+bool is_a_builtin(const char *str)
+{
+    for (int i = 0; i < 10; i++)
+        if (my_strcmp(str, my_builtins[i][0]) == 0)
+            return true;
+    return false;
+}
+
+static int my_varlen(const char *str)
+{
+    int i = 0;
+
+    while (str[i] != '\0' && str[i] != '=')
+        i++;
+    return (i);
+}
+
+int find_var_index(char *var, char **env)
+{
+    int len = my_strlen(var);
+    int big_len;
+    int i;
+
+    for (i = 0; env[i] != 0; i++) {
+        big_len = my_varlen(env[i]);
+        big_len = (len > big_len) ? len : big_len;
+        if (my_strncmp(var, env[i], big_len) == 0)
+            return (i);
+    }
+    return (-1);
+}
+
+char *my_strcat_freeleft(const char *left, const char *right)
+{
+    size_t len = my_strlen(left);
+    char *new = malloc(len + my_strlen(right) + 1);
+
+    if (!left || !right || !new)
+        return (NULL);
+    my_strcpy(new, left);
+    my_strcpy(&new[len], right);
+    free((char *) left);
+    return (new);
+}
+
+void free_path(char **path)
+{
+    for (size_t i = 0; path[i] != NULL; i++)
+        free(path[i]);
+    free(path);
+}
+
+int find_where_path(char *command, char *env[])
+{
+    char **path = my_str_towordarray(env[find_var_index("PATH", env)], ":");
+    char *str = NULL;
+    int occ = 0;
+
+    for (size_t i = 0; path[i] != NULL; i++) {
+        str = my_strcat_nofree(path[i], "/");
+        if (str == NULL)
+            continue;
+        str = my_strcat_freeleft(str, command);
+        if (str == NULL)
+            continue;
+        if (access(str, F_OK) == 0) {
+            puts(str);
+            occ++;
+        }
+        free(str);
+    }
+    free_path(path);
+    return occ;
+}
+
+void my_where(int argc, char *argv[], char *env[])
+{
+    int occ = 0;
+
+    if (argc < 2)
+        my_printf("%s: Too few arguments.\n", argv[0]);
+    for (size_t i = 1; i < (size_t) argc; i++) {
+        if (is_a_builtin(argv[i]) == true) {
+            my_printf("%s is a shell built-in\n");
+            occ++;
+        }
+        occ += find_where_path(argv[i], env);
+        if (occ == 0)
+            my_printf("%s: Command not found.\n", argv[i]);
+        occ = 0;
+    }
+}
+
+void find_which_path(char *command, char *env[])
+{
+    char **path = my_str_towordarray(env[find_var_index("PATH", env)], ":");
+    char *str = NULL;
+
+    for (size_t i = 0; path[i] != NULL; i++) {
+        str = my_strcat_nofree(path[i], "/");
+        if (str == NULL)
+            continue;
+        str = my_strcat_freeleft(str, command);
+        if (str == NULL)
+            continue;
+        if (access(str, F_OK) == 0) {
+            puts(str);
+            free(str);
+            return free_path(path);
+        }
+        free(str);
+    }
+    free_path(path);
+    my_printf("%s: Command not found.\n", command);
+}
+
+void my_which(int argc, char *argv[], char *env[])
+{
+    if (argc < 2)
+        my_printf("%s: Too few arguments.\n", argv[0]);
+    for (size_t i = 1; i < (size_t) argc; i++) {
+        if (is_a_builtin(argv[i]) == true)
+            my_printf("%s: shell built-in command.\n");
+        else
+            find_which_path(argv[i], env);
+    }
+}
 
 int main(int argc, char *argv[], char *env[])
 {
-    (void) argc;
-    (void) argv;
-    loop_shell(init_my_env(env));
-    return (0);
+    if (argc < 2)
+        return 84;
+    if (my_strcmp(argv[1], "builtins") == 0) {
+        display_builtins();
+    }
+    if (my_strcmp(argv[1], "which") == 0) {
+        my_which(argc - 1, argv + 1, env);
+    }
+    if (my_strcmp(argv[1], "where") == 0) {
+        my_where(argc - 1, argv + 1, env);
+    }
 }
