8f03727d3c505de040056fe6a119195f42a60754
Kylian Balan <kylian.balan@epitech.eu>
2019-05-23 20:03:38 +0200

    correct multiple &&&&&&&&&&&&

--- src/tokenizer/check_separators.c
+++ src/tokenizer/check_separators.c
@@ -12,8 +12,7 @@
 
 int check_sep(struct token_node *head)
 {
-    if (((head->id == ID_AND && head->prev) || head->id == ID_OR)
-&& !head->next)
+    if (head->id == ID_OR && !head->next)
         return fprintf(stderr, "Invalid null command.\n"), -1;
     if (head->id == ID_AND && head->prev &&
 head->prev->id != ID_AND && head->prev->id != ID_NOWAIT
@@ -23,6 +22,6 @@ head->prev->id != ID_AND && head->prev->id != ID_NOWAIT
     if ((head->id == ID_OR && ((!head->prev) ||
 (head->prev->id != ID_TEXT || (head->next->id != ID_TEXT
 && (head->next->id < 7 || head->next->id > 12))))))
-        return fprintf(stderr, "Invalid null command.\n"), -1;
+        return fprintf(stderr, "Invalid null command.3\n"), -1;
     return 0;
 }
