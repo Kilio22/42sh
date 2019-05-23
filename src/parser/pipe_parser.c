c23a815942df7d2dc53e3e963df89a60f2a4088f
Kylian Balan <kylian.balan@epitech.eu>
2019-05-23 15:53:07 +0200

    add good pipe parser

--- src/parser/pipe_parser.c
+++ src/parser/pipe_parser.c
@@ -6,6 +6,7 @@
 */
 
 #include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
 #include "tokenizer.h"
 #include "parser.h"
@@ -24,9 +25,11 @@ int check_pipe(struct token_node *head)
 int pipe_parser(struct cmd_s *cmd)
 {
     for (; cmd; cmd = cmd->next) {
+        cmd->pipe = pipe_token_list(cmd->token_list);
+        if (check_pipe_redirections(cmd->pipe) == -1)
+            return -1;
         if (check_pipe(cmd->token_list) == -1)
             return -1;
-        cmd->pipe = pipe_token_list(cmd->token_list);
     }
     return 0;
 }
