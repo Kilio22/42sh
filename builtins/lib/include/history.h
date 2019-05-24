1627e77d6a2e0b175883fa201c1ab86fb547564f
Antoine Gosselin <agosselin@localhost.localdomain>
2019-05-24 20:34:48 +0200

    update history

--- builtins/lib/include/history.h
+++ builtins/lib/include/history.h
@@ -21,7 +21,8 @@ int save_history(struct breakpoints_s *historic);
 void free_history(struct breakpoints_s *historic);
 int add_history(char *buff, struct breakpoints_s *historic);
 char *find_history(struct breakpoints_s *historic, char *buff);
-int replace_str_history(struct token_node *node, struct breakpoints_s *historic);
+int replace_str_history(struct token_node *node,
+struct breakpoints_s *historic);
 
 
 #endif /* !HISTORY_H_ */
