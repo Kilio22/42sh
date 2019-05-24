e74589f821e811813a722a24352334d7f43a98f4
Antoine Gosselin <agosselin@localhost.localdomain>
2019-05-24 21:34:28 +0200

    add setenv

---
+++ builtins/lib/include/setenv.h
@@ -0,0 +1,13 @@
+/*
+** EPITECH PROJECT, 2019
+** PSU_42sh_2018
+** File description:
+** setenv
+*/
+
+#ifndef SETENV_H_
+#define SETENV_H_
+
+int is_alphanum(char *str);
+
+#endif /* !SETENV_H_ */
---
+++ builtins/src/setenv/str_valid.c
@@ -0,0 +1,34 @@
+/*
+** EPITECH PROJECT, 2019
+** PSU_minishell1_2018
+** File description:
+** str_valid
+*/
+
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include "my.h"
+#include "minishell.h"
+
+static int is_alphanumeric(char c)
+{
+    if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+        return (1);
+    if (c >= '0' && c <= '9')
+        return (1);
+    else
+        return (0);
+}
+
+int is_alphanum(char *str)
+{
+    int count = 0;
+
+    while (str[count] != '\0') {
+        if (is_alphanumeric(str[count]) == 0 && str[count] != '_')
+            return (0);
+        count++;
+    }
+    return (1);
+}
