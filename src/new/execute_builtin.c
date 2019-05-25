970fc50e9ec0485a3cf4c62c9d9ec3a32ca0ed61
Kylian Balan <kylian.balan@epitech.eu>
2019-05-25 10:00:53 +0200

    add execute builtins

---
+++ src/new/execute_builtin.c
@@ -0,0 +1,23 @@
+/*
+** EPITECH PROJECT, 2019
+** PSU_42sh_2018
+** File description:
+** execute_builtins
+*/
+
+#include "parser.h"
+#include "shell.h"
+
+int exec_builtin(char **av, struct my_shell *shell)
+{
+    ssize_t idx = get_builtin_idx(av[0]);
+
+    if (idx < 0)
+        return -1;
+    if (builtins[idx]->ptr(shell, av) == -1) {
+        shell->n_return = 2;
+        return -1;
+    }
+    shell->n_return = 0;
+    return 0;
+}
