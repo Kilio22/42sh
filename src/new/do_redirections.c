12e9a5f3f284f6c382d886d68f16c3032e5002e0
Kylian Balan <kylian.balan@epitech.eu>
2019-05-25 11:20:02 +0200

    add other redirection

--- src/new/do_redirections.c
+++ src/new/do_redirections.c
@@ -5,11 +5,15 @@
 ** do_redirections
 */
 
+#include <unistd.h>
 #include <fcntl.h>
+#include <string.h>
+#include <errno.h>
+#include "my_string.h"
 #include "parser.h"
 #include "shell.h"
 
-static void do_right_redirections(struct pipe_s *pipes, int i)
+static int do_right_redirections(struct pipe_s *pipes, int i)
 {
     int fd;
 
@@ -18,35 +22,77 @@ static void do_right_redirections(struct pipe_s *pipes, int i)
     else
         fd = open(pipes->redirections[i], O_CREAT | O_RDWR | O_TRUNC, 0664);
     if (fd == -1) {
-        fprintf(stderr, "Redirection failed.\n");
-        return;
+        fprintf(stderr, "%s: %s\n", pipes->redirections[i], strerror(errno));
+        return -1;
     }
     pipes->fd[1] = fd;
+    if (i == 0 || i == 2)
+        pipes->fd[2] = fd;
+    return 0;
 }
 
-static void do_left_redirections(struct pipe_s *pipes, int i)
+static char *get_input_line(void)
+{
+    char *line = NULL;
+
+    if (isatty(STDIN_FILENO))
+        write(STDOUT_FILENO, "? ", 2);
+    line = get_line(stdin);
+    return line;
+}
+
+static int do_db_lredir(struct pipe_s *pipes, int i)
+{
+    int pipefd[2];
+    char *line = NULL;
+    char *full_line = get_input_line();
+
+    if (!full_line || strcmp(full_line, pipes->redirections[i]))
+        return 0;
+    while ((line = get_input_line()) != NULL ||
+strcmp(line, pipes->redirections[i])) {
+        full_line = my_strcat(full_line, "\n");
+        full_line = my_strcat(full_line, line);
+    }
+    if (pipe(pipefd) == -1)
+        return fprintf(stderr, strerror(errno)), -1;
+    if (write(pipefd[1], full_line, strlen(full_line)) != strlen(full_line))
+        return fprintf(stderr, strerror(errno)), -1;
+    if (close(pipefd[1]) == -1)
+        return fprintf(stderr, strerror(errno)), -1;
+    pipes->fd[0] = pipefd[0];
+    free(full_line);
+    return 0;
+}
+
+static int do_left_redirections(struct pipe_s *pipes, int i)
 {
     int fd;
 
-    if (i >= 5)
-        fd = open(pipes->redirections[i], O_CREAT | O_RDWR | O_APPEND, 0664);
-    else
-        fd = open(pipes->redirections[i], O_CREAT | O_RDWR | O_TRUNC, 0664);
+    if (i == 4)
+        return do_db_lredir(pipes, i);
+    fd = open(pipes->redirections[i], O_RDONLY);
     if (fd == -1) {
-        fprintf(stderr, "Redirection failed.\n");
-        return;
+        fprintf(stderr, "%s: %s\n", pipes->redirections[i], strerror(errno));
+        return -1;
     }
     pipes->fd[0] = fd;
+    return 0;
 }
 
-void check_redirections_files(struct pipe_s *pipes)
+int check_redirections_files(struct pipe_s *pipes)
 {
+    int n_val = 0;
+
     for (int i = 0; i < 6; i++) {
         if (!pipes->redirections[i])
             continue;
-        else if (i <= 4)
-            do_right_redirections(pipes, i);
+        else if (i < 4)
+            n_val = do_right_redirections(pipes, i);
         else
-            do_left_redirections(pipes, i);
+            n_val = do_left_redirections(pipes, i);
+        if (n_val != 0)
+            return -1;
     }
+    return 0;
 }
