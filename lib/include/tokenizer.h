03dc72269b92243a8766045dd25319b0d03f37db
HeyShafty <nathan.lecorchet@epitech.eu>
2019-05-26 13:34:36 +0200

    Fixed s/dquotes in parsing

--- lib/include/tokenizer.h
+++ lib/include/tokenizer.h
@@ -21,7 +21,7 @@ struct token_node *create_token_list_from_line(char *line);
 
 struct token_node *create_token_list_head(void);
 struct token_node *create_node(enum delim_ids id, struct token_node *prev);
-int add_node(struct token_node *head, enum delim_ids id, char *ptr, size_t n);
+int add_node(struct token_node *head, enum delim_ids id, char *ptr, ssize_t n);
 void delete_token_node_list(struct token_node *head);
 void delete_node(struct token_node *ptr);
 
